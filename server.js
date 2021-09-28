79c1bd0347e66d4a7814763a138771c045abe132
soohyun <soohyun_kim2@tmax.co.kr>
2021-09-28 14:58:44 +0900

    prisma crud

--- server.js
+++ server.js
@@ -18,24 +18,23 @@ const typeDefs = gql`
   }
   type Mutation {
     createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: Int!): Boolean
+    deleteMovie(id: Int!): Movie
+    updateMovie(id: Int!, year: Int!): Movie
   }
 `;
 
 const resolvers = {
   Query: {
     movies: () => client.movie.findMany(),
-    movie: () => ({ title: 'hello', year: 2021 }),
+    movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
   },
   Mutation: {
     createMovie: (_, { title, year, genre }) =>
       client.movie.create({
         data: { title, year, genre },
       }),
-    deleteMovie: (_, { id }) => {
-      console.log(title);
-      return true;
-    },
+    deleteMovie: (_, { id }) => client.movie.delete({ where: { id } }),
+    updateMovie: (_, { id, year }) => client.movie.update({ where: { id }, data: { year } }),
   },
 };
 
