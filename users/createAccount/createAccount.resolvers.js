4fa80c6df3eb3819e7ffc21eea2e591c3b72d308
soohyun <soohyun_kim2@tmax.co.kr>
2021-10-13 22:44:57 +0900

    create account return fixed

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,19 +1,27 @@
+import bcrypt from "bcrypt";
 import client from "../../client";
-import bcrpyt from "bcrypt";
 
 export default {
   Mutation: {
     createAccount: async (_, { firstName, lastName, username, email, password }) => {
       try {
-        // check if username or email are already on DB.
         const existingUser = await client.user.findFirst({
-          where: { OR: [{ username }, { email }] },
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
         });
         if (existingUser) {
           throw new Error("This username/password is already taken.");
         }
-        const uglyPassword = await bcrpyt.hash(password, 10); // hash password
-        return client.user.create({
+        const uglyPassword = await bcrypt.hash(password, 10);
+        await client.user.create({
           data: {
             username,
             email,
@@ -22,9 +30,14 @@ export default {
             password: uglyPassword,
           },
         });
-        // save and return the user
+        return {
+          ok: true,
+        };
       } catch (e) {
-        return e;
+        return {
+          ok: false,
+          error: "Cant create account.",
+        };
       }
     },
   },
