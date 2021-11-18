ee88847990f969e7166232f757a92c37c2156cb5
soohyun <soohyun_kim2@tmax.co.kr>
2021-11-18 16:16:42 +0900

    no playground

--- src/server.js
+++ src/server.js
@@ -12,8 +12,6 @@ const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
-  playground: true,
-  introspection: true,
   context: async (ctx) => {
     if (ctx.req) {
       return {
